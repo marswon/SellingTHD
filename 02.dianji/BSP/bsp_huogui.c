90379e3e1a9e7f059170dd42986ad3c0e5c2e80a
MCU <18004023826@163.com>
2018-02-26 14:25:36 +0800

    去掉掉货检测部分，因为实际运行中有异响

--- 02.dianji/BSP/bsp_huogui.c
+++ 02.dianji/BSP/bsp_huogui.c
@@ -150,7 +150,7 @@ u8 HUOWU_Take(u8 m, u8 n)
         if(flag_times > 100)     //结束时一般大于100
         {
             //开启掉货检测
-            Enable_duishe();
+//            Enable_duishe();      //目前有异响，关闭掉货检测
         }
 
         if((LINEFB1 == 1 && flag_line == 1) || (LINEFB2 == 1 && flag_line == 2) || (LINEFB3 == 1 && flag_line == 3) || (LINEFB4 == 1 && flag_line == 4) || (LINEFB5 == 1 && flag_line == 5) || (LINEFB6 == 1 && flag_line == 6)
